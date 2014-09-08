615dfee327bd471d43475fe63cf7033a0afb4922
brandon-adams <brandon.adams@newwave-technologies.com>
2014-09-08 09:47:57 -0400

    Adding new parse.py

--- public/jenkins/parse.py
+++ public/jenkins/parse.py
@@ -3,10 +3,11 @@ from xml.etree.ElementTree import Element
 from xml.etree.ElementTree import SubElement
 import sys
 import os
-#import yaml
+import yaml
 import json
 import time
 import string
+import argparse
 import subprocess
 
 def get_server_info():
@@ -22,30 +23,55 @@ def get_server_info():
     #print output
     retval = p.wait()
 
+def build_manifest(details):
 
-#job_file = 'job-template.xml'
-#git_url = 'https://github.com/brandon-adams/helloworld-demo.git'
-#node_name = 'yKPgpR-mngd-AppNode'
-#cell_name = 'yKPgpR-dmgr-Cell'
-#server_name = 'GnOEpf-srvr'
-#war_file = 'was-service-broker-0.1.0.war'
-#war_file = 'helloworld.war'
-#output_file = 'demo-job.xml'
+    template = "template_manifest.yml"
+    output_file = "ex_manifest.yml"
+    #template = details["template_file"]
+    #output_file = details["output_file"]
+    in_stream = open(template, "r")
+    manifest = yaml.load(in_stream)
+    
+    #app_name = "hello"
+    #memory = "128M"
+    #inst = 1
+    #disk = "128M"
+    #host = "hello"
+    #path = "$WORKSPACE/cf_demoapp_ruby_rack/"
+    #timeout = 120
+    #command = ""
+    #buildpack = "Ruby"
 
-def main():
-    #job_info = str(sys.argv[0])
-    #with open('job-info.yml', 'r') as f:
-    #    details = yaml.load(f)
-    details = []
-    #get_server_info()
-    template(details)
-    send_job(details)
+    app_name = details["app_name"]
+    memory = details["memory"]
+    inst = details["inst"]
+    disk = details["disk"]
+    host = details["host"]
+    path = details["path"]
+    #timeout = details["app_name"]
+    #command = details["app_name"]
+    buildpack = details["buildpack"]
 
-def template(details):
+    manifest["applications"][0]["name"] = app_name
+    manifest["applications"][0]["memory"] = memory
+    manifest["applications"][0]["instances"] = inst
+    manifest["applications"][0]["disk_quota"] = disk
+    manifest["applications"][0]["host"] = host
+    manifest["applications"][0]["path"] = "$WORKSPACE/" + path
+    #manifest["applications"][0]["timeout"] = timeout
+    #manifest["applications"][0]["command"] = command
+    if manifest["applications"][0]["buildpack"] != 'None':
+        manifest["applications"][0]["buildpack"] = buildpack
+
+    out_stream = open(output_file, "w")
+    yaml.dump(manifest, out_stream, explicit_start=True, default_flow_style=False)
+    #print yaml.dump(manifest, explicit_start=True, default_flow_style=False)
+
+def json_template(details):
     #job_file = details["variables"]["job_file"]
     job_file = "%s/no-webs.xml" % "/Users/Marc/dev/code/meteorite/ba_demo_NEW/public/jenkins"
-    #git_url = details["variables"]["git_url"]
-    git_url = str(sys.argv[2])
+    git_url = details["git_url"]
+    #git_url = str(sys.argv[2])
     #node_name = details["variables"]["node_name"]
     #cell_name = details["variables"]["cell_name"]
     #server_name = details["variables"]["server_name"]
@@ -93,13 +119,11 @@ def run_command(command):
         print line,
     retval = p.wait()
 
-def send_job(details):
+def create_job(details):
 
     #output_file = details["variables"]["output_file"]
     output_file = "%s/demo-job.xml" % "/Users/Marc/dev/code/meteorite/ba_demo_NEW/public/jenkins"
-    #job_name = details["variables"]["job_name"]
-    # job_name = str(sys.argv[0])
-    job_name = "test_App"
+    job_name = details["app_name"]
 
     command = "java -jar /Users/Marc/dev/code/meteorite/ba_demo_NEW/public/jenkins/jenkins-cli.jar -s http://192.168.0.126:8090/ create-job %s < %s" % (job_name, output_file) 
     run_command(command)
@@ -112,9 +136,49 @@ def send_job(details):
     print "Done build?"
     time.sleep(10)
     
-    #print "Deleting..."
-    #command = "java -jar jenkins-cli.jar -s http://192.168.0.126:8090/ delete-job helloworld-demo"
-    #run_command(command)
-    #time.sleep(5)
+def delete_job(details):
+
+    #job_name = details["variables"]["job_name"]
+    job_name = "test_App"
+    print "Deleting..."
+    command = "java -jar jenkins-cli.jar -s http://192.168.0.126:8090/ delete-job %s" % job_name
+    run_command(command)
+    time.sleep(5)
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Launch some apps to Jenkins and CF. Purely backend.')
     
+    parser.add_argument('name', help='Name of application to launch in CF. Also used for Jenkins job.')
+    parser.add_argument('git_url', help='Github URL for project')
+    parser.add_argument('--num_inst', nargs='?', type=int, default='1', help='Number of instances to launch in CF.')
+    parser.add_argument('--mem', nargs='?', default='512M', help='Amount of memory to use in CF. Defaults to 512M.')
+    parser.add_argument('--disk', nargs='?', default='512M', help='Amount of disk space to use in CF. Defaults to 512M.')
+    parser.add_argument('--buildpack', nargs='?', help='Override buildpack use in CF here.')
+    #parser.add_argument('--host', nargs='?', default='', help='Hostname for the application in CF.')
+    parser.add_argument('--path', nargs='?', default='$WORKSPACE/build/libs/*.war', help='Path to the WAR file.')
+    parser.add_argument('--start', action='store_true', help='Set to 1 to launch in CF.')
+    args = parser.parse_args()
+
+    details = {}
+    details["app_name"] = args.name
+    details["memory"] = args.mem
+    details["inst"] = args.num_inst
+    details["disk"] = args.disk
+    details["host"] = args.name
+    details["path"] = args.path
+    details["buildpack"] = args.buildpack
+    details["start"] = args.start
+    
+    if args.start:
+        print "launching"
+    print details
+    #in_stream = open(file_name, "r")
+    #app_details = yaml.load(in_stream)
+    #get_server_info()
+    build_manifest(details)
+    #json_template(details)
+    #create_job(details)
+    #delete_job(details)
+
 main()
